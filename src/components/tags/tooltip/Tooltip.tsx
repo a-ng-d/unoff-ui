fcbc16cbd22f5d5b11979ee107ef7bc39a16555a
@a-ng-d <a.grimaud@involt.io>
2026-03-30 01:04:16 +0200

    fix(tooltip): simplify tooltip arrow positioning and cleanup CSS

--- src/components/tags/tooltip/Tooltip.tsx
+++ src/components/tags/tooltip/Tooltip.tsx
@@ -97,8 +97,7 @@ const Tooltip = (props: TooltipProps) => {
         className="tooltip__block"
         style={{
           transform: `translateX(${shift}px)`,
-          '--_shift': `${shift}px`,
-        } as React.CSSProperties}
+        }}
         role="presentation"
       >
         <div
